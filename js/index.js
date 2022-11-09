78ae39b8c282c5f280a68e93d3eaea002a6befe0
“Adilgit config --global user.name “Adil <adilkalbaev2004@gmail.com>
2022-11-09 12:37:29 +0600

    adaptive navbar and main

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+let hamburger = document.querySelector(".hamburger");
+let logo = document.querySelector(".logo__a");
+
+hamburger.addEventListener("click", function () {
+
+  hamburger.classList.toggle("is-active");
+  logo.classList.toggle("is-active");
+});
