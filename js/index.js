0eaf8a8e2c1fdc318f02d99338c5eedef36fd99d
“Adilgit config --global user.name “Adil <adilkalbaev2004@gmail.com>
2022-11-16 11:41:05 +0600

    add on up

--- js/index.js
+++ js/index.js
@@ -6,3 +6,30 @@ hamburger.addEventListener("click", function () {
   logo.classList.toggle("is-active");
 });
 
+let overlay = document.querySelector(".overlay");
+let none = document.querySelector(".auth-toggle");
+let auth = document.querySelector(".form_auth_block");
+let exit = document.querySelector(".form_auth_exit");
+let dark = document.querySelector(".dark-btn");
+
+none.addEventListener("click", function (e) {
+  none.classList.toggle("is-active");
+  auth.classList.toggle("is-active");
+  overlay.classList.toggle("is-active");
+  exit.classList.toggle("is-active");
+});
+overlay.addEventListener("click", function () {
+  none.classList.remove("is-active");
+  auth.classList.remove("is-active");
+  overlay.classList.toggle("is-active");
+  exit.classList.toggle("is-active");
+});
+exit.addEventListener("click", function () {
+  none.classList.remove("is-active");
+  auth.classList.remove("is-active");
+  overlay.classList.toggle("is-active");
+  exit.classList.toggle("is-active");
+});
+dark.addEventListener("click",function(){
+  document.body.classList.toggle("dark")
+})
